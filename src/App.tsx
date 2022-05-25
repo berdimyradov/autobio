6bf34bc317235d0f7ae49303cba20ce29eadc0e8
Kerim Berdimyradov <k.berdimyradov@sdventures.com>
2022-05-25 14:58:39 +0300

    :tada Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import meImage from './assets/me.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={meImage} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>Berdimyradov Kerim</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/berdimyradov/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
         </a>
       </header>
     </div>
